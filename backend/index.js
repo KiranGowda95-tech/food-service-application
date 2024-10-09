2bda96f2cf980de793f402bf17e100890a740374
Kiran <kirankumardn74@gmail.com>
2024-10-09 11:55:50 +0530

    files updated

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,31 @@
-console.log("hello");
 const express = require("express");
 const mongoose = require("mongoose");
-const getDishes = require("./controllers/dishesControllers");
-const router = require("./routes/userRoutes");
+const dishes = require("./routes/dishRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const PORT = 5000;
-app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://kirankumardn74:kumar123@devconnector.fmvtn9e.mongodb.net/?retryWrites=true&w=majority&appName=DevConnector"
-).then(()=>{
-    console.log('Connected to Atlas database')
-}).catch((err)=>{
-    console.log(err)
+mongoose
+  .connect(
+    "mongodb+srv://kirankumardn74:kumar123@devconnector.fmvtn9e.mongodb.net/?retryWrites=true&w=majority&appName=DevConnector"
+  )
+  .then(() => {
+    console.log("Connected to Atlas database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+//middleware
+app.use((req, res, next) => {
+  console.log("Time:", Date.now());
+  next();
 });
 
-app.use(router);
-
-//app.get("/api/dishes", getDishes);
-
-// app.post('/api/user/register',(req,res)=>{
-//     console.log(req.body)
-//     res.send('Okey')
-// })
+app.use("/api", dishes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}`);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router=express.Router()
 const {registerUser,loginUser}=require("../controllers/userController")
 
-router.post("/user/register",registerUser);
-router.post("/user/login",loginUser)
+router.post("/register",registerUser);
+router.post("/login",loginUser)
 
 module.exports=router
